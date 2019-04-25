93e4edbb8bcba45f1f63c65711da32209630e3a1
JawedOuk <jawed.oukili@gmail.com>
2019-04-25 15:36:27 +0200

    first build

--- SneakersApp/SneakersApp.Data/Models/Shoes.cs
+++ SneakersApp/SneakersApp.Data/Models/Shoes.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace SneakersApp.Data.Models
 {
-    class Shoes
+    public class Shoes
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Created { get; set; }
+        public string Url { get; set; }
+        public IEnumerable<string> Tags { get; set; }
+        `public string Status { get; set; }
     }
 }
