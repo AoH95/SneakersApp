73da6c5473726331e49bda46f74a8e7fd863a6d2
JawedOuk <jawed.oukili@gmail.com>
2019-05-01 13:59:15 +0200

    enable unit test

---
+++ SneakersApp/SneakersApp.XUnit/BasicTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using SneakersApp;
+
+namespace SneakersApp.xUnit
+{
+    public class BasicTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        public BasicTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        public async Task GetHttpRequest(string url)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString()
+                );
+        }
+    }
+}
